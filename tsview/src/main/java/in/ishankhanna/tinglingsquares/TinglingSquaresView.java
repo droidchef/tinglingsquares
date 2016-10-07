6b236c5cc580d347edbc513d69bb8ec6aad64a0a
Ishan Khanna <ishan1604@gmail.com>
2016-10-08 00:08:18 +0200

    feat: A better anim

--- tsview/src/main/java/in/ishankhanna/tinglingsquares/TinglingSquaresView.java
+++ tsview/src/main/java/in/ishankhanna/tinglingsquares/TinglingSquaresView.java
@@ -1,18 +1,16 @@
 package in.ishankhanna.tinglingsquares;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Ishan Khanna
@@ -20,6 +18,8 @@ import java.util.List;
 
 public class TinglingSquaresView extends FrameLayout {
 
+    public static final int LEFT_TO_RIGHT_ANIM_TIME = 400;
+    public static final float THRESHOLD_TO_TRIGGER_NEXT_ANIMATION = 0.7f;
     SquareView[][] squareViews = new SquareView[3][4];
 
     private Context ctx;
@@ -29,6 +29,11 @@ public class TinglingSquaresView extends FrameLayout {
     private int w,h;
     private boolean moveFromLeftToRight = true;
     private PropertyValuesHolder propertyValuesHolder;
+
+    private ObjectAnimator square1, square2, square3, square4, square5, square6, square7, square8,
+            square9, square10, square11, square12;
+
+    private AnimatorSet column1Animation,column2Animation,column3Animation,column4Animation;
 
     public TinglingSquaresView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -63,6 +68,139 @@ public class TinglingSquaresView extends FrameLayout {
                 squareViews[m][n] = new SquareView(ctx, m, n);
             }
         }
+
+        propertyValuesHolder = PropertyValuesHolder.ofFloat(View.ROTATION, 0, 90);
+
+        square1 = ObjectAnimator.ofPropertyValuesHolder(squareViews[0][0], propertyValuesHolder);
+        square2 = ObjectAnimator.ofPropertyValuesHolder(squareViews[0][1], propertyValuesHolder);
+        square3 = ObjectAnimator.ofPropertyValuesHolder(squareViews[0][2], propertyValuesHolder);
+        square4 = ObjectAnimator.ofPropertyValuesHolder(squareViews[0][3], propertyValuesHolder);
+//        square1.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 200);
+//        square2.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 150);
+//        square3.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 50);
+//        square4.setDuration(LEFT_TO_RIGHT_ANIM_TIME);
+
+        square5 = ObjectAnimator.ofPropertyValuesHolder(squareViews[1][0], propertyValuesHolder);
+        square6 = ObjectAnimator.ofPropertyValuesHolder(squareViews[1][1], propertyValuesHolder);
+        square7 = ObjectAnimator.ofPropertyValuesHolder(squareViews[1][2], propertyValuesHolder);
+        square8 = ObjectAnimator.ofPropertyValuesHolder(squareViews[1][3], propertyValuesHolder);
+//        square5.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 200);
+//        square6.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 150);
+//        square7.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 100);
+//        square8.setDuration(LEFT_TO_RIGHT_ANIM_TIME);
+
+        square9 = ObjectAnimator.ofPropertyValuesHolder(squareViews[2][0], propertyValuesHolder);
+        square10 = ObjectAnimator.ofPropertyValuesHolder(squareViews[2][1], propertyValuesHolder);
+        square11 = ObjectAnimator.ofPropertyValuesHolder(squareViews[2][2], propertyValuesHolder);
+        square12 = ObjectAnimator.ofPropertyValuesHolder(squareViews[2][3], propertyValuesHolder);
+//        square10.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 150);
+//        square11.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 50);
+//        square9.setDuration(LEFT_TO_RIGHT_ANIM_TIME - 200);
+//        square12.setDuration(LEFT_TO_RIGHT_ANIM_TIME);
+
+        column4Animation = new AnimatorSet();
+        column4Animation.play(square4).with(square12);
+
+        column3Animation = new AnimatorSet();
+        column3Animation.play(square3).with(square11);
+
+        column2Animation = new AnimatorSet();
+        column2Animation.play(square2).with(square10);
+
+        column1Animation = new AnimatorSet();
+        column1Animation.play(square1).with(square9);
+
+        column1Animation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                setupAnimationTriggerListeners();
+            }
+        });
+
+        setupAnimationTriggerListeners();
+
+    }
+
+    private void setupAnimationTriggerListeners() {
+        square8.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                if (animation.getAnimatedFraction() > 0.25f) {
+                    column4Animation.start();
+                    square8.removeAllUpdateListeners();
+                }
+
+            }
+        });
+
+
+        square7.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                if (animation.getAnimatedFraction() > 0.25f) {
+                    column3Animation.start();
+                    square7.removeAllUpdateListeners();
+                }
+
+            }
+        });
+
+        square6.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                if (animation.getAnimatedFraction() > 0.25f) {
+                    column2Animation.start();
+                    square6.removeAllUpdateListeners();
+                }
+
+            }
+        });
+
+        square5.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                if (animation.getAnimatedFraction() > 0.25f) {
+                    column1Animation.start();
+                    square5.removeAllUpdateListeners();
+                }
+
+            }
+        });
+
+        square4.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (animation.getAnimatedFraction() > 0.75f) {
+                    square7.start();
+                    square4.removeAllUpdateListeners();
+                }
+            }
+        });
+
+        square3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (animation.getAnimatedFraction() > 0.75f) {
+                    square6.start();
+                    square3.removeAllUpdateListeners();
+                }
+            }
+        });
+
+        square2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (animation.getAnimatedFraction() > 0.75f) {
+                    square5.start();
+                    square2.removeAllUpdateListeners();
+                }
+            }
+        });
     }
 
     @Override
@@ -77,187 +215,22 @@ public class TinglingSquaresView extends FrameLayout {
         }
     }
 
-    private Runnable col1Runnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int row=0;row<3;row++) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[row][0], propertyValuesHolder);
-                switch (row) {
-                    case 0:
-                        animator.setDuration(800);
-                        break;
-                    case 1:
-                        animator.setDuration(700);
-                        break;
-                    case 2:
-                        animator.setDuration(600);
-                        break;
-                }
-                animators.add(animator);
-            }
-            animatorSet.playTogether(animators);
-            animatorSet.start();
-        }
-    };
-
-    private Runnable col2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int row=0;row<3;row++) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[row][1], propertyValuesHolder);
-                switch (row) {
-                    case 0:
-                        animator.setDuration(800);
-                        break;
-                    case 1:
-                        animator.setDuration(700);
-                        break;
-                    case 2:
-                        animator.setDuration(600);
-                        break;
-                }
-                animators.add(animator);
-            }
-            animatorSet.playTogether(animators);
-            animatorSet.start();
-        }
-    };
-
-    private Runnable col3Runnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int row=0;row<3;row++) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[row][2], propertyValuesHolder);
-                switch (row) {
-                    case 0:
-                        animator.setDuration(800);
-                        break;
-                    case 1:
-                        animator.setDuration(700);
-                        break;
-                    case 2:
-                        animator.setDuration(600);
-                        break;
-                }
-                animators.add(animator);
-            }
-            animatorSet.playTogether(animators);
-            animatorSet.start();
-        }
-    };
-
-    private Runnable col4Runnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int row=0;row<3;row++) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[row][3], propertyValuesHolder);
-                switch (row) {
-                    case 0:
-                        animator.setDuration(800);
-                        break;
-                    case 1:
-                        animator.setDuration(700);
-                        break;
-                    case 2:
-                        animator.setDuration(600);
-                        break;
-                }
-                animators.add(animator);
-            }
-            animatorSet.playTogether(animators);
-            animatorSet.start();
-        }
-    };
-
     public void runAnimation() {
         if (moveFromLeftToRight) {
             propertyValuesHolder = PropertyValuesHolder.ofFloat(View.ROTATION, 0, 90);
             moveFromLeftToRight = false;
-            post(middleRowRunnable);
-            post(topRowRunnable);
-            post(bottomRowRunnable);
-
+            post(animationRunnable);
         } else {
             propertyValuesHolder = PropertyValuesHolder.ofFloat(View.ROTATION, 90, 0);
             moveFromLeftToRight = true;
-            int baseDelay = 500;
-            postDelayed(col1Runnable, baseDelay);
-            postDelayed(col2Runnable, baseDelay + 50);
-            postDelayed(col3Runnable, baseDelay + 100);
-            postDelayed(col4Runnable, baseDelay + 150);
+            post(animationRunnable);
         }
     }
 
-    private Runnable middleRowRunnable = new Runnable() {
+    private Runnable animationRunnable = new Runnable() {
         @Override
         public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int col=3;col>=0;col--) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[1][col], propertyValuesHolder);
-                animator.setInterpolator(new AccelerateInterpolator(2));
-                setDurationForAnimator(col, animator);
-                animators.add(animator);
-            }
-            animatorSet.playSequentially(animators);
-            animatorSet.start();
-        }
-    };
-
-    private Runnable topRowRunnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int col=3;col>=0;col--) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[0][col], propertyValuesHolder);
-                animator.setInterpolator(new AccelerateInterpolator(2));
-                setDurationForAnimator(col, animator);
-                animators.add(animator);
-            }
-            animatorSet.playSequentially(animators);
-            animatorSet.start();
-        }
-    };
-
-    private void setDurationForAnimator(int col, ObjectAnimator animator) {
-        switch (col) {
-            case 0:
-                animator.setDuration(475);
-                break;
-            case 1:
-                animator.setDuration(450);
-                break;
-            case 2:
-                animator.setDuration(425);
-                break;
-            case 3:
-                animator.setDuration(400);
-                break;
-        }
-    }
-
-    private Runnable bottomRowRunnable = new Runnable() {
-        @Override
-        public void run() {
-            AnimatorSet animatorSet = new AnimatorSet();
-            List<Animator> animators = new ArrayList<>();
-            for (int col=3;col>=0;col--) {
-                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(squareViews[2][col], propertyValuesHolder);
-                animator.setInterpolator(new AccelerateInterpolator(2));
-                setDurationForAnimator(col, animator);
-                animators.add(animator);
-            }
-            animatorSet.playSequentially(animators);
-            animatorSet.start();
+            square8.start();
         }
     };
 
